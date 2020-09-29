39ee7879fb2489ad84d1ed296e64e03f9f974a67
Sircoward <misterballians@gmail.com>
2020-09-29 17:39:13 +0200

    fini

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,23 +4,49 @@ class Movies extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            cardmovie: []
-        }
+            cardmovie: [],
+            error: '',
+            loading: true
+        };
     }
 
     componentDidMount(){
         fetch('https://api.themoviedb.org/3/movie/popular?api_key=3b35e8b15a0c59c01dff8e1c5e8af97f&language=en-US&page=1')
-        .then((response) => {return response.json()})
-        .then((data) =>  {this.setState({cardmovie: data.results}); console.log(data);})
+        .then((response) => {
+            if(response.status === 200){
+                return response.json();     
+            }else if(response.status === 404){
+                this.setState({ error: true })
+            }
+        })
+        .then((data) => {
+                this.setState({cardmovie: data.results, loading: false})
+            })
+        .catch((error) => {
+                this.setState({ error: true })
+            })
+
     }
 
     render(){
         const cardMovie = this.state.cardmovie.map((data, i) => <div key={i}><img src={`https://image.tmdb.org/t/p/w200${data.poster_path}`} /><p>{data.title}</p></div>)
+         if(this.state.error){ 
+             return (
+             <p>Ooops, something went wrong</p>
+             ) 
+        }else if(this.state.loading){
+            return(
+                <p>Loading...</p>
+            )
+        }else{
         return(
+            
             <main className="wrapper" >
                 {cardMovie}
             </main>
         )
+        }
+
     }
 }
 
--- src/components/Series.js
+++ src/components/Series.js
@@ -4,24 +4,47 @@ class Series extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            cardserie: []
+            cardserie: [],
+            error: '',
+            loading: true
         }
     }
 
     componentDidMount(){
         fetch('https://api.themoviedb.org/3/tv/popular?api_key=3b35e8b15a0c59c01dff8e1c5e8af97f&language=en-US&page=1')
-        .then((response) => {return response.json()})
-        .then((data) =>  {this.setState({cardserie: data.results}); console.log(data);})
+        .then((response) => {
+            if(response.status === 200){
+                return response.json();     
+            }else if(response.status === 404){
+                this.setState({ error: true })
+            }
+        })
+        .then((data) => {
+            this.setState({cardserie: data.results, loading: false})
+        })
+        .catch((error) => {
+            this.setState({ error: true })
+        })
     }
 
     render(){
         const cardSerie = this.state.cardserie.map((data, i) => <div key={i}><img src={`https://image.tmdb.org/t/p/w200${data.poster_path}`} /><p>{data.name}</p></div>)
-        
-        return(
-            <main  className="wrapper">
-                {cardSerie}
-            </main>
-        )
+        if(this.state.error){ 
+            return (
+            <p>Ooops, something went wrong</p>
+            ) 
+       }else if(this.state.loading){
+           return(
+               <p>Loading...</p>
+           )
+       }else{
+       return(
+           
+           <main className="wrapper" >
+               {cardMovie}
+           </main>
+       )
+       }
     }
 }
 
